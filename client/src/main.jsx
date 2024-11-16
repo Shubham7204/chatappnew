8437e537313ce375ba70b2c11acedfbcb50a216e
Shubham7204 <onlinelearner01learn@gmail.com>
2024-11-16 22:52:53 +0530

    deployment issues

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { AuthProvider } from "./context/AuthProvider.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { SocketProvider } from "./context/SocketContext.jsx";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AuthProvider>
