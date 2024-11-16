6e459bcd589452da1397517564b9eb65b3875d27
Shubham7204 <onlinelearner01learn@gmail.com>
2024-11-17 00:40:34 +0530

    commit

--- server/SocketIO/server.js
+++ server/SocketIO/server.js
@@ -1,17 +1,13 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
-import dotenv from "dotenv";
-
-// Load environment variables
-dotenv.config();
 
 const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:3001", // fallback included
+    origin: "http://localhost:3001",
     methods: ["GET", "POST"],
   },
 });
@@ -23,6 +19,7 @@ export const getReceiverSocketId = (receiverId) => {
 
 const users = {};
 
+// used to listen events on server side.
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
   const userId = socket.handshake.query.userId;
@@ -30,8 +27,10 @@ io.on("connection", (socket) => {
     users[userId] = socket.id;
     console.log("Hello ", users);
   }
+  // used to send the events to all connected users
   io.emit("getOnlineUsers", Object.keys(users));
 
+  // used to listen client side events emitted by server side (server & client)
   socket.on("disconnect", () => {
     console.log("a user disconnected", socket.id);
     delete users[userId];
