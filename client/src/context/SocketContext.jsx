4ee33b48643b586e303097444be40b19dfc23d5d
Shubham7204 <onlinelearner01learn@gmail.com>
2024-11-16 23:04:12 +0530

    check

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chatappnew-1bng.onrender.com", {
+      const socket = io(" https://chatappnew.vercel.app", {
         query: {
           userId: authUser.user._id,
         },
