025ce1c0bdb6f4e4cdeb47176173652c06fce8b0
Shubham7204 <onlinelearner01learn@gmail.com>
2024-11-18 10:25:20 +0530

    pushing for deployment

--- server/index.js
+++ server/index.js
@@ -3,28 +3,42 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import userRoute from "./routes/user.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server } from "./SocketIO/server.js";
 
 dotenv.config();
 
-// middleware
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+// MongoDB Connection
 const URI = process.env.MONGODB_URI;
-
-mongoose.connect(URI)
+mongoose
+    .connect(URI)
     .then(() => console.log("Connected to MongoDB"))
     .catch((error) => console.log("MongoDB connection error:", error));
 
-//routes
+// API Routes
 app.use("/api/user", userRoute);
 app.use("/api/message", messageRoute);
 
+// Serve React Client
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
+// Start Server
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
-    console.log(`Server is Running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
